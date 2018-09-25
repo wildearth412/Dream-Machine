347e48406c4ba39575ed650b69e046ecdc910533
oukenshu <oukenshu@team-lab.com>
2018-09-25 20:23:27 +0900

    update project  /   add GearRotation.cs

---
+++ DreamMachine/Assets/Scripts/Linkage/GearRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+//[DefaultExecutionOrder(1)]
+[RequireComponent(typeof(RotationConstraint))]
+public class GearRotation : MonoBehaviour
+{
+    public bool isActive;
+    public float weight = 1.0f;
+    public bool x;
+    public bool y;
+    public bool z;
+    public int axisLinkIdx = 4;       // 0 - x2x, 1 - y2x, 2 - z2x, 3 - x2y, 4 - y2y, 5 - z2y, 6 - x2z, 7 - y2z, 8 - z2z
+    private RotationConstraint rc;
+    [SerializeField]
+    private Vector3 currentOffset;
+
+    private void Start()
+    {
+        this.rc = GetComponent<RotationConstraint>();
+        this.rc.weight = weight;
+    }
+
+    private void Update ()
+    {
+        if(isActive)
+        {
+            Quaternion rotation = this.rc.GetSource(0).sourceTransform.rotation;
+            currentOffset = new Vector3(rotation.eulerAngles.x * (x ? -2.0f : 1.0f), rotation.eulerAngles.y * (y ? -2.0f : 1.0f), rotation.eulerAngles.z * (z ? -2.0f : 1.0f));
+            this.rc.rotationOffset = currentOffset;
+        }
+    }
+
+    public void SetWeight(float w)
+    {
+        this.rc.weight = w;
+    }
+}
